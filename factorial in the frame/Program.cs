7175b9dbafbeff2f83f915724a362c9c7c5f4858
FedorShuvalov <shuvalov.fedor18@outlook.com>
2020-10-15 14:20:33 +0500

    Добавьте файлы проекта.

---
+++ factorial in the frame/Program.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Threading;
+
+namespace factorial_in_the_frame
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+                Console.WriteLine("Введите какой нужно расчитать фактариал:");
+                int fact = ReadFact();
+
+                ColorChange(fact);
+        }
+
+        static int ReadFact()
+        {
+            bool isSuccess;
+            int fact;
+
+            do
+            {
+                isSuccess = int.TryParse(Console.ReadLine(), out fact);
+
+                if (isSuccess == false)
+                    Console.WriteLine("Неверный ввод");
+
+            } while (!isSuccess);
+
+            return FindFact(fact);
+        }
+
+        static int FindFact(int endFact)
+        {
+            int fact = 1;
+            for (int i = 1; i <= endFact; i++)
+            {
+                fact *= i;
+            }
+            return fact;
+        }
+
+        static void ColorChange(int fact)
+        {
+            while (true)
+            {
+                int time = 1000;
+
+                Console.BackgroundColor = ConsoleColor.White;
+                Console.Clear();
+                Console.ForegroundColor = ConsoleColor.Red;
+                PrintBox(fact);
+                Thread.Sleep(time);
+
+                Console.BackgroundColor = ConsoleColor.Red;
+                Console.Clear();
+                Console.ForegroundColor = ConsoleColor.White;
+                PrintBox(fact);
+                Thread.Sleep(time);
+
+            }
+
+            static void PrintBox(int fact)
+        {
+            string strFact = Convert.ToString(fact);
+
+            PrintLine("╔", "═", "╗",strFact);
+            PrintNumberLine("║",strFact, "║");
+            PrintLine("╚", "═", "╝", strFact);
+        }
+
+        static void PrintLine(string leftLine,string middleLine,string rightLine,string strFact)
+        {
+
+            Console.Write(leftLine);
+            for (int i = 0; i < strFact.Length; i++)
+            {
+                Console.Write(middleLine);
+            }
+            Console.WriteLine(rightLine);
+        }
+
+        static void PrintNumberLine(string leftLine, string middleLine, string rightLine)
+        {
+            Console.WriteLine($"║{middleLine}║");;
+        }
+        }
+    }
+}

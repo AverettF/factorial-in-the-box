8193e5a4bb98a25c69c3ff8e865f86e99bfed399
FedorShuvalov <shuvalov.fedor18@outlook.com>
2020-10-15 19:34:09 +0500

    "оптимилированная" рабочая версия
    Что было сделано:
    1)Теперь программа вводит 3 линии,к которыз запоминается таблица.Поэтому программе достаточно вывести две строки,вместо того ,чтобы каждый раз отрисовывать по новой.
    
    Что бы я сделал,но мне лень:
    1)сделать красиво

--- factorial in the frame/Program.cs
+++ factorial in the frame/Program.cs
@@ -4,15 +4,22 @@ using System.Threading;
 namespace factorial_in_the_frame
 {
     class Program
-    {
-        public const int time = 300;
+    {           
+        
+        
+        //ВАЖНО:Данное решение "оптимизированно",но не является лучшим вариантом для проверки,т.к. довольно запутанно.
+        //Поэтому было бы лучше выбрать прошлую версию
+
+        public const int time = 10;
 
         static void Main(string[] args)
         {
                 Console.Write("Введите какой нужно расчитать фактариал:\n!");
                 uint fact = ReadFact();
 
-                PrintColorBox(fact);
+                string line1, line2, line3;
+                FindLineBox(fact,out line1, out line2, out line3);
+                PrintColorBox(line1, line2, line3);
         }
 
         static uint ReadFact()
@@ -39,91 +46,105 @@ namespace factorial_in_the_frame
             return endFact * FindFact(endFact - 1);
         }
 
-        static void PrintColorBox(uint fact)
+        static void FindLineBox(uint fact,out string line1, out string line2, out string line3)
+        {
+            string strFact = Convert.ToString(fact);
+
+            line1 = SaveLine("╔", "═", "╗", strFact);
+            line2 = SaveNumberLine("║", strFact, "║", strFact);
+            line3 = SaveLine("╚", "═", "╝", strFact);
+        }
+
+        static string SaveLine(string leftLine, string middleLine, string rightLine, string strFact)
+        {
+            string line;
+
+            line = GetMiddleWidth(strFact);
+            line += leftLine;
+            for (int i = 0; i < strFact.Length; i++)
+            {
+                line += middleLine;
+            }
+            line += rightLine;
+
+            return line;
+        }
+
+        static string SaveNumberLine(string leftLine, string middleLine, string rightLine, string strFact)
+        {
+            string line;
+            line = GetMiddleWidth(strFact);
+            line += $"║{middleLine}║";
+            return line;
+
+        }
+
+        static string GetMiddleWidth(string strFact)
+        {
+            string line = "";
+            for (int i = 0; i < (Console.WindowWidth - strFact.Length + 2) / 2; i++)
+            {
+                line += " ";
+            }
+            return line;
+        }
+
+        static void PrintColorBox(string line1,string line2,string line3)
         {
             while (true)
             {
                 Console.BackgroundColor = ConsoleColor.Black;
                 Console.Clear();
                 Console.ForegroundColor = ConsoleColor.White;
-                PrintBox(fact);
+                PrintBox(line1, line2, line3);
                 Thread.Sleep(time);
 
                 Console.BackgroundColor = ConsoleColor.DarkGray;
                 Console.Clear();
                 Console.ForegroundColor = ConsoleColor.Gray;
-                PrintBox(fact);
+                PrintBox(line1, line2, line3);
                 Thread.Sleep(time);
 
                 Console.BackgroundColor = ConsoleColor.Gray;
                 Console.Clear();
                 Console.ForegroundColor = ConsoleColor.DarkGray;
-                PrintBox(fact);
+                PrintBox(line1, line2, line3);
                 Thread.Sleep(time);
 
                 Console.BackgroundColor = ConsoleColor.White;
                 Console.Clear();
                 Console.ForegroundColor = ConsoleColor.Black;
-                PrintBox(fact);
+                PrintBox(line1, line2, line3);
                 Thread.Sleep(time);
 
                 Console.BackgroundColor = ConsoleColor.Gray;
                 Console.Clear();
                 Console.ForegroundColor = ConsoleColor.DarkGray;
-                PrintBox(fact);
+                PrintBox(line1, line2, line3);
                 Thread.Sleep(time);
 
                 Console.BackgroundColor = ConsoleColor.DarkGray;
                 Console.Clear();
                 Console.ForegroundColor = ConsoleColor.Gray;
-                PrintBox(fact);
+                PrintBox(line1, line2, line3);
                 Thread.Sleep(time);
             }
         }
-        static void PrintBox(uint fact)
-        {
-            string strFact = Convert.ToString(fact);
 
+        static void PrintBox(string line1, string line2, string line3)
+        {
             MiddleHeight();
-            Middle(strFact);
-            PrintLine("╔", "═", "╗", strFact);//╔═══════╗
-            Middle(strFact);
-            PrintNumberLine("║",strFact, "║");//║ Fact  ║
-            Middle(strFact);
-            PrintLine("╚", "═", "╝", strFact);//╚═══════╝
+            Console.WriteLine(line1);
+            Console.WriteLine(line2);
+            Console.WriteLine(line3);
         }
-
-        static void PrintLine(string leftLine,string middleLine,string rightLine,string strFact)
-        {
-
-            Console.Write(leftLine);
-            for (int i = 0; i < strFact.Length; i++)
-            {
-                Console.Write(middleLine);
-            }
-            Console.WriteLine(rightLine);
-        }
-
-        static void PrintNumberLine(string leftLine, string middleLine, string rightLine)
-        {
-            Console.WriteLine($"║{middleLine}║");;
-        } 
 
         static void MiddleHeight()
         {
-            for (int i = 0; i < Console.WindowHeight / 2 - 2; i++) 
+            for (int i = 0; i < Console.WindowHeight / 2 - 2; i++)
             {
                 Console.WriteLine();
             }
         }
-
-        static void Middle(string strFact)
-        {
-            for (int i = 0; i < (Console.WindowWidth - strFact.Length + 2) / 2; i++) 
-            {
-                Console.Write(" ");
-            }
-        }
-
     }
 }
